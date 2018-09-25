18ff694eb174ef5285b5a782649af6cdc45ed576
Donald Evans <donaldevans@Donalds-MacBook-Pro.local>
2018-09-25 15:36:41 -0600

    added divs and interpolation

--- js/app.js
+++ js/app.js
@@ -1,26 +1,52 @@
+let seconds = 0;
 
 class Tamagotchi {
-	constructor(name) {
-		this.name = name;
-		this.stamina = 2;
-		this.boredom = 2;
-		this.age = 0;
-		this.hunger = 2;
-		this.affection = 2;
+    constructor(name) {
+        this.name = name;
+        this.stamina = 0;
+        this.boredom = 0;
+        this.age = 0;
+        this.hunger = 0;
+        this.affection = 0;
     }
 }
-	
-let seconds = 0;
-// const timePassing = setInterval(() => {
-//     seconds++;
-//     // inside timer: hunger, age, tiredness
-//     if(seconds > 10){
-//         clearInterval(timePassing);
-//         console.log("Done");
+$('button').on('click', () => {
+    console.log('click works');
+    // When the user clicks the button, the game starts
+    startGame();
+  
+  });
+const startGame = () => {
+
+const newPet = new Tamagotchi();
+// const timePassing = () => {
+//     // $('.time').text(`${seconds}`);
+//     if (seconds%10 === 0){
+//         newPet.hunger++;
 //     }
-   
+//     if (seconds%10 === 0){
+//         newPet.sleepiness++;
+//     }
+//     if (seconds%10 === 0){
+//         newPet.boredom++;
+//     }
+//     if (seconds%10 === 0){
+//         newPet.age++;
+//     }
+//     seconds++;   
+// }
+
+// setInterval(timePassing, 1000);
 // }
 
 
 
-const newPet = new Tamagotchi(BertBert);
+$('.stamina').text(`Stamina ${newPet.stamina}`);
+$('.hunger').text(`Hunger ${newPet.hunger}`);
+$('.boredom').text(`Boredom ${newPet.boredom}`);
+$('.affection').text(`Affection ${newPet.affection}`);
+$('.age').text(`Age ${newPet.age}`);
+
+
+
+}
