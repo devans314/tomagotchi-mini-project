6a843ea884e6b833750a38d914f932cc4ab0fa6a
Donald Evans <donaldevans@Donalds-MacBook-Pro.local>
2018-09-25 12:00:13 -0600

    class complete and instantiated

--- js/app.js
+++ js/app.js
@@ -1,10 +1,26 @@
-let seconds = 0;
-const timePassing = setInterval(() => {
-    seconds++;
-    // inside timer: hunger, age, tiredness
-    if(seconds > 10){
-        clearInterval(timePassing);
-        console.log("Done");
+
+class Tamagotchi {
+	constructor(name) {
+		this.name = name;
+		this.stamina = 2;
+		this.boredom = 2;
+		this.age = 0;
+		this.hunger = 2;
+		this.affection = 2;
     }
-    if(){}
 }
+	
+let seconds = 0;
+// const timePassing = setInterval(() => {
+//     seconds++;
+//     // inside timer: hunger, age, tiredness
+//     if(seconds > 10){
+//         clearInterval(timePassing);
+//         console.log("Done");
+//     }
+   
+// }
+
+
+
+const newPet = new Tamagotchi(BertBert);
